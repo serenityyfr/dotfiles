b466ccb675663a40614b6d8c52e0a8ff5c3b2e4a
remnantserenityy <remnantserenityy@tuta.com>
2026-06-19 15:07:24 +0200

    update

--- .config/hypr/modules/keybinds.lua
+++ .config/hypr/modules/keybinds.lua
@@ -1,10 +1,10 @@
 local mainMod = "SUPER"
-local term = "alacritty"
-local menu = "rofi -show drun"
+local term    = "alacritty"
+local menu    = "rofi -show drun"
 
-hl.bind(mainMod .. " + Q", hl.dsp.window.close())
+hl.bind(mainMod .. " + Q",      hl.dsp.window.close())
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(term))
-hl.bind(mainMod .. " + Space", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + Space",  hl.dsp.exec_cmd(menu))
 
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ action = "toggle" }))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
@@ -19,10 +19,10 @@ hl.bind(mainMod .. " + SHIFT + F", hl.dsp.exec_cmd("waypaper &"))
 --**********************************************************************************
 
 -- Move focus
-hl.bind(mainMod .. " + k", hl.dsp.focus({ direction = "up" }))
-hl.bind(mainMod .. " + j", hl.dsp.focus({ direction = "down" }))
-hl.bind(mainMod .. " + h", hl.dsp.focus({ direction = "left" }))
-hl.bind(mainMod .. " + l", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + k",         hl.dsp.focus({       direction = "up" }))
+hl.bind(mainMod .. " + j",         hl.dsp.focus({       direction = "down" }))
+hl.bind(mainMod .. " + h",         hl.dsp.focus({       direction = "left" }))
+hl.bind(mainMod .. " + l",         hl.dsp.focus({       direction = "right" }))
 
 -- Move windows
 hl.bind(mainMod .. " + SHIFT + k", hl.dsp.window.move({ direction = "up" }))
@@ -30,11 +30,8 @@ hl.bind(mainMod .. " + SHIFT + j", hl.dsp.window.move({ direction = "down" }))
 hl.bind(mainMod .. " + SHIFT + h", hl.dsp.window.move({ direction = "left" }))
 hl.bind(mainMod .. " + SHIFT + l", hl.dsp.window.move({ direction = "right" }))
 
--- Bind windows to numrow
 for i = 1, 10 do
-    local key = i % 10 -- overflow 10 back to 0
+    local key = i % 10
     hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
     hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
-
-
--- .config/hypr/modules/windowrules.lua
+++ .config/hypr/modules/windowrules.lua
@@ -14,3 +14,11 @@ hl.window_rule({
         match     = { class = "alacritty" },
         workspace = "10",
 })
+
+hl.window_rule({
+        name      = "reading",
+        match     = { title = "zathura" },
+        float     = true,
+        center    = true,
+        size      = {"monitor_w*0.5", "monitor_h*0.9"},
+})
